cdda5ccf071be19e2f3958f74c90739bd3690530
Rolland Henri <rolland.h.dev@gmail.com>
2024-07-30 15:49:49 +0200

    Site full Js & Sass Jour 1

--- script/index.js
+++ script/index.js
@@ -2,21 +2,23 @@
 
 const navBar = document.createElement("nav");
 const header = document.createElement("header");
+const main = document.createElement("main");
 
 /*------ Create Block ------*/
 
 document.body.appendChild(navBar);
 document.body.appendChild(header);
+document.body.appendChild(main);
 
 /*------ InnerHTML ------*/
 
 navBar.innerHTML = `
 <div class="logo"> </div>
 <ul> 
-    <li> Accueil </li>
-    <li> About </li>
-    <li> Skill </li>
-    <li> Contact </li>
+    <li><a href="#">Accueil</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#">Skill</a></li>
+    <li><a href="#">Contact</a></li>
 </ul>
 `;
 
@@ -25,6 +27,15 @@ header.innerHTML = `
 <p> Est <span> 
  
 <i class="fa-brands fa-sass"></i> </span> </p> 
+`;
+
+main.innerHTML = `
+<section id="about">
+<div class="aboutTitle"> <h2> A propos de moi </h2> 
+<p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate minus, ut officia, quasi eos voluptas eum nulla libero provident aliquid, nobis neque dolorum suscipit. Sequi odio, explicabo labore sunt eius atque perspiciatis quo porro fugiat ad distinctio, quam autem excepturi. </p>
+</div>
+<div class="aboutImg"> </div>
+</section> 
 `;
 
 /*------ Annim ------*/
