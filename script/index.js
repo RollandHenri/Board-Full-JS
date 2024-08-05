5195082c0080c3a6e4d32431d3f077b11399b8a3
Rolland Henri <rolland.h.dev@gmail.com>
2024-08-05 10:49:26 +0200

    site full javascript jour 3

--- script/index.js
+++ script/index.js
@@ -3,7 +3,8 @@
 const nav = document.querySelector("nav");
 const header = document.querySelector("header");
 const aboutTitle = document.querySelector(".aboutTitle");
-console.log(aboutTitle);
+const aboutImg = document.querySelector(".aboutImg");
+const skills = document.getElementById("skills");
 
 /*------ Create HTML ------*/
 
@@ -11,9 +12,10 @@ nav.innerHTML = `
 <div class="logo"> </div>
 
 <ul>
-<li> Accueil </li>
-<li> About </li>
+<li> <a href="#"> Accueil </a> </li>
+<li> <a href='#about'> About </a> </li>
 <li> Skils </li>
+<li> Folio </li>
 <li> Contact </li>
 </ul>
 `;
@@ -28,6 +30,55 @@ aboutTitle.innerHTML = `
 <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt deserunt nostrum est laboriosam, unde ullam modi impedit necessitatibus iure, ab neque dolorem consequatur? Voluptate, dignissimos recusandae deleniti cumque nihil iste? </p>
 `;
 
+skills.innerHTML = `
+<h2> Mes Skills </h2>
+<div class='skillsContain'>
+<div class="skillsLeft"> 
+    <h3> Hard Skills </h3>
+        <div class="iconLeft"> 
+        <div class='iconFlex'>
+            <i class="fa-brands fa-html5"></i>
+            <p> HTML 5 </p>
+        </div>
+        <div class='iconFlex'>
+            <i class="fa-brands fa-css3-alt"></i>
+            <p> CSS 3 </p>
+        </div>
+        <div class='iconFlex'> 
+           <i class="fa-brands fa-sass"></i>
+            <p> SASS </p>
+        </div>
+        <div class='iconFlex'> 
+            <i class="fa-brands fa-js"></i>
+            <p> JavaScript </p>
+        </div>
+        </div> 
+</div> 
+<div class="skillsRight">
+    <h3> Soft Skills </h3>
+    <div class="iconRight"> 
+    <div class='iconFlex'> 
+        <i class="fa-brands fa-github"></i>
+        <p> GitHub </p>
+    </div>
+    <div class='iconFlex'> 
+        <i class="fa-brands fa-figma"></i>
+        <p> Figma </p>
+    </div>
+    <div class='iconFlex'>
+        <i class="fa-brands fa-spotify"></i>
+        <p> Spotify </p>
+    </div>
+     <div class='iconFlex'>
+        <i class="fa-solid fa-mug-saucer"></i>
+        <p> Café </p>
+    </div>
+    </div>
+</div> 
+</div> 
+
+`;
+
 /*------ EventListener ------*/
 
 window.addEventListener("scroll", () => {
@@ -38,4 +89,38 @@ window.addEventListener("scroll", () => {
   }
 });
 
-/*------ Observer ------*/
+/*------ Observer translate X ------*/
+
+const observer = new IntersectionObserver(
+  (entrer) => {
+    for (const entry of entrer) {
+      if (entry.isIntersecting) {
+        entry.target.style.transform = "translateX(0px)";
+        entry.target.style.opacity = "1";
+      }
+    }
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+observer.observe(aboutImg);
+
+/*----- Observer Translate Y ------*/
+
+const observerII = new IntersectionObserver(
+  (enter) => {
+    for (const entry of enter) {
+      if (entry.isIntersecting) {
+        entry.target.style.transform = "translateY(0px)";
+        entry.target.style.opacity = "1";
+      }
+    }
+  },
+  {
+    threshold: 1,
+  }
+);
+
+observerII.observe(skills);
